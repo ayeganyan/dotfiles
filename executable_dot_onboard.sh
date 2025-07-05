0e35dc07f31df27c3f70d396c711478828ba0100
Armen Yeganyan <ayeganyan@Armens-MacBook-Pro.local>
2025-07-05 11:10:38 +0200

    Fixed onboarding

--- executable_dot_onboard.sh
+++ executable_dot_onboard.sh
@@ -1,17 +1,13 @@
 #!/bin/zsh
 
-# Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 # Install sdkman
 curl -s "https://get.sdkman.io" | bash
 
 # Install zim
-curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+brew install zim
 
-zimfw install
+zimfw install 
 
-source ~/.zshrc
 
 # Install useful utlities
 
@@ -24,9 +20,9 @@ brew install --cask iterm2 raycast
     cd /tmp && \
     git clone git@github.com:mbadolato/iTerm2-Color-Schemes.git && \
     cd iTerm2-Color-Schemes.git && \
-    tools/import-scheme.sh schemes/* &
+    tools/import-scheme.sh schemes/*
 )
 
 # Install dev tools
-brew install --cask cursor vscode jetbrains-toolbox
+brew install --cask cursor visual-studio-code jetbrains-toolbox
 
